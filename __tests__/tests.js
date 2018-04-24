6c5de979221e04951f32ff72474ca81434409012
Nishant <nishantsalhotra.ns@gmail.com>
2018-04-24 23:29:33 +0530

    Exercise: add segment 1 exercises

---
+++ __tests__/tests.js
@@ -0,0 +1,238 @@
+const {
+  factorial,
+  counter,
+  counterFactory,
+  limitFunctionCallCount,
+  cacheFunction,
+  applyOperator,
+  isEven,
+  isOdd,
+  booWho,
+  sumFibs,
+  sumPrimes,
+  rectangleDiagonal,
+  isTriangle,
+} = require('../src');
+
+describe('factorial', () => {
+  test('should return a number', () => {
+    expect(typeof factorial(0)).toEqual('number');
+    expect(factorial(5)).toEqual(120);
+    expect(factorial(20)).toEqual(2432902008176640000);
+  });
+});
+
+describe('applyOperator', () => {
+  test('should return 0 when no operands are specified', () => {
+    expect(applyOperator('+')).toEqual(0);
+  });
+
+  test('should sum when +, -, *, /, % operator is applied', () => {
+    expect(applyOperator('+', 1, 2, 3, 4, 5)).toEqual(15);
+    expect(applyOperator('+', 1)).toEqual(1);
+
+    expect(applyOperator('-', 1, 2, 3, 4, 5)).toEqual(-15);
+    expect(applyOperator('-', 1)).toEqual(-1);
+
+    expect(applyOperator('*', 1, 2, 3, 4, 5)).toEqual(120);
+    expect(applyOperator('*', 1)).toEqual(1);
+
+    expect(applyOperator('/', 1, 2, 3, 4, 5)).toEqual(15);
+    expect(applyOperator('/', 1)).toEqual(1);
+
+    expect(applyOperator('%', 27, 13, 3)).toEqual(1);
+    expect(applyOperator('%', 21)).toEqual(21);
+  });
+
+  test('should throw error when operator is not specified', () => {
+    expect(applyOperator()).toThrowError();
+  });
+});
+
+describe('isEven', () => {
+  it('should return true if even number is sent as argument', () => {
+    expect(isEven(4)).toBe(true);
+  });
+
+  it('should return false if odd number is sent as argument', () => {
+    expect(isEven(51)).toBe(false);
+  });
+});
+
+describe('sumFibs', () => {
+  test('should return a number', () => {
+    expect(typeof sumFibs(1)).toEqual('number');
+  });
+
+  test('should return the correct number', () => {
+    expect(sumFibs(1000)).toBe(1785);
+    expect(sumFibs(4000000)).toBe(4613732);
+    expect(sumFibs(4)).toBe(5);
+    expect(sumFibs(75024)).toBe(60696);
+  });
+});
+
+describe('counter', () => {
+  it('should return a function', () => {
+    expect(typeof counter()).toBe('function');
+  });
+  it('should return 1 when the returned function is invoked', () => {
+    expect(counter()()).toBe(1);
+  });
+  it('should increment and return the number each time the function is invoked', () => {
+    const counterFunction = counter();
+    expect(counterFunction()).toBe(1);
+    expect(counterFunction()).toBe(2);
+    expect(counterFunction()).toBe(3);
+    expect(counterFunction()).toBe(4);
+    expect(counterFunction()).toBe(5);
+  });
+});
+
+describe('counterFactory', () => {
+  it('should return an object with an increment method and a decrement method', () => {
+    expect(typeof counterFactory().increment).toBe('function');
+    expect(typeof counterFactory().decrement).toBe('function');
+  });
+  it('should have a working increment method that increments the private counter and returns the new value', () => {
+    const myCounter = counterFactory();
+    expect(myCounter.increment()).toBe(1);
+    expect(myCounter.increment()).toBe(2);
+    expect(myCounter.increment()).toBe(3);
+    expect(myCounter.increment()).toBe(4);
+  });
+  it('should have a working decrement method that decrements the private counter and returns the new value', () => {
+    const myCounter = counterFactory();
+    expect(myCounter.decrement()).toBe(-1);
+    expect(myCounter.decrement()).toBe(-2);
+    expect(myCounter.decrement()).toBe(-3);
+    expect(myCounter.decrement()).toBe(-4);
+  });
+});
+
+describe('isOdd', () => {
+  it('should return true if odd number is sent as argument', () => {
+    expect(isOdd(41)).toBe(true);
+  });
+
+  it('should return false if even number is sent as argument', () => {
+    expect(isOdd(2)).toBe(false);
+  });
+});
+
+describe('limitFunctionCallCount', () => {
+  it('should return a function', () => {
+    expect(typeof limitFunctionCallCount()).toBe('function');
+  });
+  it('should return a wrapped version of the original function that can only be invoked n times', () => {
+    const foo = () => (true);
+    const limitedFunction = limitFunctionCallCount(foo, 2);
+    expect(limitedFunction()).toBe(true);
+    limitedFunction();
+    expect(limitedFunction()).toBe(null);
+  });
+  it('should properly handle arguments in the wrapped function', () => {
+    const foo = (x, y, z) => (x + y + z);
+    const limitedFunction = limitFunctionCallCount(foo, 2);
+    expect(limitedFunction(5, 10, 15)).toBe(30);
+    limitedFunction(0, 0, 0);
+    expect(limitedFunction()).toBe(null);
+  });
+});
+
+describe('isTriangle', () => {
+  it('should return true if triangle can be formed given three lines', () => {
+    expect(isTriangle(3, 4, 5)).toBe(true);
+  });
+
+  it('should return false if triangle can not be formed given three lines', () => {
+    expect(isTriangle(1, 4, 2)).toBe(false);
+  });
+});
+
+describe('cacheFunction', () => {
+  it('should return a function', () => {
+    expect(typeof cacheFunction()).toBe('function');
+  });
+  it('The cached function should return the correct result', () => {
+    const foo = x => (x * x);
+    const cachedFunction = cacheFunction(foo);
+    expect(cachedFunction(5)).toBe(25);
+  });
+  it('should cache function results and not rerun the original callback if the same arguments are presented', () => {
+    const foo = jest.fn();
+    const myCachedFunction = cacheFunction(foo);
+    myCachedFunction(true);
+    myCachedFunction(true);
+    myCachedFunction(true);
+    myCachedFunction(true);
+    myCachedFunction(true);
+    myCachedFunction(10);
+    myCachedFunction(10);
+    myCachedFunction(10);
+    myCachedFunction(10);
+    myCachedFunction(10);
+    expect(foo).toHaveBeenCalledTimes(2);
+  });
+});
+
+describe('rectangleBlock', () => {
+  it('calculates rectangle diagonal', () => {
+    expect(rectangleDiagonal(3, 4)).toBe(5);
+  });
+});
+
+describe('Logical operators', () => {
+  test('(a && b) || c', () => {
+    expect(('a' && 'b') || 'c').toBe('replace this');
+  });
+
+  test('(a || b) && c', () => {
+    expect(('a' || 'b') && 'c').toBe('replace this');
+  });
+
+  test('null && (b || c)', () => {
+    expect(null && ('b' || 'c')).toBe('replace this');
+  });
+
+  test('a && (null || c)', () => {
+    expect('a' && (null || 'c')).toBe('replace this');
+  });
+
+  test('a && (b || null)', () => {
+    expect('a' && ('b' || null)).toBe('replace this');
+  });
+
+  test('a || (b && c)', () => {
+    expect('a' || ('b' && 'c')).toBe('replace this');
+  });
+
+  test('null || (b && c)', () => {
+    expect(null || ('b' && 'c')).toBe('replace this');
+  });
+
+  test('a || (null && c)', () => {
+    expect('a' || (null && 'c')).toBe('replace this');
+  });
+});
+
+describe('sumPrimes', () => {
+  test('should return a number', () => {
+    expect(typeof sumPrimes(10)).toEqual('number');
+  });
+  test('should return the correct output', () => {
+    expect(sumPrimes(10)).toBe(17);
+    expect(sumPrimes(977)).toBe(73156);
+  });
+});
+
+describe('booWho', () => {
+  test('should return the correct expected output', () => {
+    expect(booWho(true)).toBe(true);
+    expect(booWho(false)).toBe(true);
+    expect(booWho([1, 2, 3])).toBe(false);
+    expect(booWho(NaN)).toBe(false);
+    expect(booWho('a')).toBe(false);
+    expect(booWho('true')).toBe(false);
+  });
+});
---
+++ src/index.js
@@ -0,0 +1,127 @@
+/* eslint-disable no-unused-vars */
+
+/**
+ * Read this for factorial: https://en.wikipedia.org/wiki/Factorial
+ */
+function factorial(num) {
+
+}
+
+/**
+ * Return a function that when invoked increments and returns a counter variable.
+ * Example: const newCounter = counter();
+ * newCounter(); // 1
+ * newCounter(); // 2
+ */
+function counter() {
+
+}
+
+// Return an object that has two methods called `increment` and `decrement`.
+// `increment` should increment a counter variable in closure scope and return it.
+// `decrement` should decrement the counter variable and return it.
+function counterFactory() {
+
+}
+
+// Return a true or false for wether a triangle can be formed using the three lines
+function isTriangle(a, b, c) {
+
+}
+
+// Should return a function that invokes `cb`.
+// The returned function should only allow `cb` to be invoked `n` times.
+function limitFunctionCallCount(cb, n) {
+
+}
+
+// Should return a function that invokes `cb`.
+// A cache should be kept in closure scope.
+// The cache should keep track of all arguments have been used to invoke this function.
+// If the returned function is invoked with arguments that it has already seen
+// then it should return the cached result and not invoke `cb` again.
+// `cb` should only ever be invoked once for a given set of arguments.
+function cacheFunction(cb) {
+
+}
+
+/**
+ * The function takes an operator and applies it to the
+ * n given arguments.
+ *
+ * Example: applyOperator('+', 1,2,3,4,5) => 15
+ *
+ */
+function applyOperator() {
+
+}
+
+/**
+ * Do this without using the % operator.
+ */
+function isOdd(num) {
+
+}
+
+/**
+ * Do this without using the % operator.
+ */
+function isEven(num) {
+
+}
+
+/**
+ * Check if a value is classified as a boolean primitive. Return true or false.
+ * Boolean primitives are true and false.
+ */
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+}
+
+/**
+ * Given a positive integer num, return the sum of all odd Fibonacci
+ * numbers that are less than or equal to num.
+ * The first two numbers in the Fibonacci sequence are 1 and 1.
+ * Every additional number in the sequence is the sum of the two
+ * previous numbers. The first six numbers of the Fibonacci sequence
+ * are 1, 1, 2, 3, 5 and 8.
+ * For example, sumFibs(10) should return 10 because all odd Fibonacci
+ * numbers less than 10 are 1, 1, 3, and 5
+ */
+function sumFibs(num) {
+
+}
+
+/**
+ * Sum all the prime numbers up to and including the provided number.
+ * A prime number is defined as a number greater than one and having
+ * only two divisors, one and itself. For example, 2 is a prime
+ * number because it's only divisible by one and two.
+ * The provided number may not be a prime.
+ */
+function sumPrimes(num) {
+
+}
+
+/**
+ * Return the length of diagonal, given the length of sides of rectangle
+ */
+function rectangleDiagonal(length, height) {
+
+}
+
+module.exports = {
+  factorial,
+  counter,
+  counterFactory,
+  limitFunctionCallCount,
+  cacheFunction,
+  applyOperator,
+  isEven,
+  isOdd,
+  booWho,
+  sumFibs,
+  sumPrimes,
+  rectangleDiagonal,
+  isTriangle,
+};
