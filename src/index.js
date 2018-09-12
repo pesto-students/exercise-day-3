977c87f01bf97c4bdef741ef99fcbbc67c2a218f
Dibyajyoti Ghosal <dibyajyotighosal@ACA80103.ipt.aol.com>
2018-09-13 03:26:24 +0530

    Completed more functions

--- src/index.js
+++ src/index.js
@@ -67,14 +67,22 @@ function cacheFunction(cb) {
  * Example: applyOperator('+', 1,2,3,4,5) => 15
  *
  */
-function applyOperator() {
-
+function applyOperator(sign) {
+  if(sign === "+"){
+    
+  }
 }
 
 /** Q8 (*)
  * Do this without using the % operator.
  */
 function isOdd(num) {
+  for(i=1;i<num;i+=2){
+    if(num==i){
+      return true;
+    }
+    else return false;
+  }
 
 }
 
@@ -82,7 +90,12 @@ function isOdd(num) {
  * Do this without using the % operator.
  */
 function isEven(num) {
-
+  for(i=0;i<num;i+=2){
+    if(num==i){
+      return true;
+    }
+    else return false;
+  }
 }
 
 /** Q10 (*)
@@ -90,6 +103,9 @@ function isEven(num) {
  * Boolean primitives are true and false.
  */
 function booWho(bool) {
+  if(typeof(bool) === "boolean")
+  return true;
+  else return false;
   // What is the new fad diet for ghost developers? The Boolean.
 }
 
@@ -116,16 +132,16 @@ function sumFibs(num) {
  */
 function sumPrimes(num) {
   let sum=0;
-  for(let i=0;i<=num;i++){
-    let isPrime = false;
+  for(let i=2;i<=num;i++){
+    let isPrime = 1;
     for(let j=2;j<=i-1;j++){
       if(i%j === 0){
-        isPrime = false;
+        isPrime = 0;
         break;
       }
       
     }
-    if(isPrime === true){
+    if(isPrime === 1){
       sum +=i; 
     }
    
