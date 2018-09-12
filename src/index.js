041f67e9e6892aa615fb522cde7aeebae2e6b896
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-09-13 03:29:44 +0530

    Fixed reverseString

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@
  * Don't use Array.prototype.reverse
  */
 function reverseString(string) {
-
+  let reversed = '';
+  for (let i = string.length - 1; i >= 0; i -= 1) reversed += string[i];
+  return reversed;
 }
 
 /** (*)
